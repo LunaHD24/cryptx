bbdc52bdf6361a561d8b78cd96125dfdd200514b
LunaHD24 <yannick.rumelfanger@gmail.com>
2023-04-28 15:23:40 +0200

    Made Importable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from filymanaging import __version__
+
+setup(
+    name='filemanaging',
+    version=__version__,
+
+    url='https://github.com/LunaHD24/FilyManaging',
+    author='LunaHD24',
+    author_email='yannick.rumelfanger@gmail.com',
+
+    py_modules=['filymanaging'],
+)
